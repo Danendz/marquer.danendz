40df27d2f7d228c9b1a3c7f7ff316128aef18164
danila <danilapayne@gmail.com>
2025-12-17 10:28:37 +0500

    added prefix

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::get('/me', [UserController::class, 'me']);
+    Route::get('/marquer/me', [UserController::class, 'me']);
 });
